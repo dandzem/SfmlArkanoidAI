11df9209395fbec47b529f8cfa33d5563e665b29
Daniil <113435148+dandzem@users.noreply.github.com>
2022-11-07 12:54:15 +0200

    Update ArkanoidSFML.cpp

--- src/ArkanoidSFML.cpp
+++ src/ArkanoidSFML.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : ArkanoidSFML.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
